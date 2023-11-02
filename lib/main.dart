21e2cd37b35a1245bc88d06fdbe77ecd6afd3d98
farrelfavian18 <farrelfavian180502@mhs.mdp.ac.id>
2023-11-02 19:29:17 +0700

    finish tampilan

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'data/candi_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,16 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(),
+      home: DetailScreen(candi: candiList[0]),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
